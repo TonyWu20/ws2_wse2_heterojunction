e0504e9be87ad670edd455d1a285b5e7e567a60b
TonyWu20 <tony.w21@gmail.com>
2025-08-11 03:45:24 +0800

    new dev shell with lammps built

--- flake.nix
+++ flake.nix
@@ -6,39 +6,91 @@
   };
   outputs = { nixpkgs, my_lammps, ... }:
     let
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
-      pkgs = system: import nixpkgs {
+      pkgsFor = { system, cudaSupport }: import nixpkgs {
         inherit system; overlays = [ my_lammps.overlays.default ];
         config = {
           allowUnfree = true;
-          cudaSupport = true;
+          inherit cudaSupport;
         };
       };
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (pkgs system));
     in
     {
-      devShells = forAllSystems (
-        pkgs:
-        ({
+      devShells.x86_64-linux =
+        let
+          pkgs = pkgsFor { system = "x86_64-linux"; cudaSupport = true; };
+          gpuOptions = cudaArch: [
+            "GPU_ARCH=${cudaArch}"
+            "GPU_API=CUDA"
+            "CUDA_MPS_SUPPORT=on"
+          ];
+          setKokkosOptions = kokkosCudaArch: with pkgs.lib;[
+            (cmakeBool "Kokkos_ENABLE_OPENMP" true)
+            (cmakeBool "Kokkos_ENABLE_CUDA" true)
+            (cmakeBool "Kokkos_ARCH_${strings.toUpper kokkosCudaArch}" true)
+            (cmakeBool "Kokkos_ARCH_NATIVE" true)
+          ];
+          lammps = { cudaArch, kokkosCudaArch }: my_lammps.lammps {
+            gpuExtraOptions = gpuOptions cudaArch;
+            kokkosOptios = setKokkosOptions kokkosCudaArch;
+            inherit pkgs;
+          };
+          buildInputs = with pkgs.python313Packages; [
+            pkgs.python313
+            pip
+            ase
+            pylint-venv
+            pkgs.cudaPackages.cudatoolkit
+          ];
+        in
+        {
+          default = pkgs.mkShell {
+            inherit buildInputs;
+            packages = with pkgs; [
+              fish
+              mpi
+              (lammps
+                { cudaArch = "sm_61"; kokkosCudaArch = "pascal61"; })
+            ];
+            venvDir = "./.venv";
+          };
+          klt = pkgs.mkShell {
+            inherit buildInputs;
+            packages = with pkgs; [
+              fish
+              mpi
+              (lammps
+                { cudaArch = "sm_90"; kokkosCudaArch = "hopper90"; })
+            ];
+            venvDir = "./.venv";
+          };
+        };
+      devShells.aarch64-darwin =
+        let
+          pkgs = pkgsFor { system = "aarch64-darwin"; cudaSupport = false; };
+          gpuExtraOptions = [
+            "GPU_API=opencl"
+          ];
+          kokkosOptions = with pkgs.lib; [
+            (cmakeBool "Kokkos_ENABLE_OPENMP" true)
+            (cmakeBool "Kokkos_ARCH_NATIVE" true)
+          ];
+          lammpsMac = my_lammps.packages.aarch64-darwin.default;
+        in
+        {
           default = pkgs.mkShell {
             buildInputs = with pkgs.python313Packages; [
               pkgs.python313
               pip
               ase
               pylint-venv
-            ] ++ (
-              pkgs.lib.optional pkgs.stdenv.isLinux [
-                pkgs.cudaPackages.cudatoolkit
-              ]
-            );
+            ];
             packages = with pkgs; [
               fish
               mpi
-              lammps
+              lammpsMac
             ];
             venvDir = "./.venv";
           };
-        })
-      );
+        };
     };
 }
